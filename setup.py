89032641ad3d405369bf134bb04516cdd696bb32
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-04 20:33:01 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import re
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="ferrischat.py",
+    author="Cryptex",
+    url="https://github.com/Cryptex-github/ferrischat.py",
+    project_urls={
+        "Issue tracker": "https://github.com/Cryptex-github/ferrischat.py/new/main",
+    },
+    version="0.0.0", # 0.1.0 for the finished release
+    packages=[
+        "ferris",
+    ],
+    license="MIT",
+    description="An asynchronous python wrapper for ferris.chat api",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    extras_require={
+        "docs": [
+            "sphinx>=4.1.1",
+            "furo",
+        ],
+        "performance": [
+            "orjson>=1.3.0"
+        ]
+    },
+    python_requires=">=3.8.0",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
