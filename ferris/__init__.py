95919c1807ed4ff155103f3da2f14efb562ede27
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-10 13:24:14 -0700

    Base class and rename to ferrispy

--- ferris/__init__.py
+++ ferris/__init__.py
@@ -1,4 +1,5 @@
 from .client import Client
 
 __author__ = 'Cryptex & jay3332'
-__version__ = '0.0.0'
+__version__ = '0.0.0' 
+# 0.1.0 for the finished release
--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup
+import re
+
+with open('ferris/__init__.py', 'r') as f:
+    content = f.read()
+    try:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1) # type: ignore
+    except AttributeError:
+        raise RuntimeError('Unable to find version string')
+    
+    try:
+        author = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1) # type: ignore
+    except AttributeError:
+        author = 'Cryptex'
+
+# with open('requirements.txt', 'r') as f:
+#     requirements = f.readlines()
+
 
 with open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name="ferrischat.py",
-    author="Cryptex",
+    name="ferrispy",
+    author=author,
     url="https://github.com/Cryptex-github/ferrischat.py",
     project_urls={
         "Issue tracker": "https://github.com/Cryptex-github/ferrischat.py/new/main",
     },
-    version="0.0.0", # 0.1.0 for the finished release
+    version=version,
     packages=[
         "ferris",
     ],
@@ -19,6 +36,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
+    install_requires=["aiohttp"],
     extras_require={
         "docs": [
             "sphinx>=4.1.1",
