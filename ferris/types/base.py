a49cc29e9f404b836ea761d34b99dcdbcf765373
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-08-11 20:50:03 -0400

    Type aliases should follow PascalCase.

--- ferris/types/base.py
+++ ferris/types/base.py
@@ -3,4 +3,4 @@ from typing import Dict, Union, List, Any
 __all__ = ('Snowflake', 'DATA', )
 
 Snowflake = int
-DATA = Dict[str, Union[Dict[str, Any], List[Any], Snowflake]]
+Data = Dict[str, Union[Dict[str, Any], List[Any], Snowflake]]
