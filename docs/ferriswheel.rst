aea2b482ad9e0c943d145d28de2e4634ce84811d
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-20 16:54:08 -0700

    fix doc and document errors.py

--- docs/ferriswheel.rst
+++ docs/ferriswheel.rst
@@ -3,11 +3,13 @@ FerrisWheel API Reference
 
 Client
 ------
+
 .. autoclass:: ferris.Client
     :members:
 
 Base
 ----
+
 .. autoclass:: ferris.SnowflakeObject
     :members:
 
@@ -21,54 +23,74 @@ Base
 
 Channnel
 --------
+
 .. autoclass:: ferris.Channel
     :members:
     :inherited-members:
 
 Guild
 -----
+
 .. autoclass:: ferris.Guild
     :members:
     :inherited-members:
 
 Member
 ------
+
 .. autoclass:: ferris.Member
     :members:
     :inherited-members:
 
 Message
 -------
+
 .. autoclass:: ferris.Message
     :members:
     :inherited-members:
 
 User
 ----
+
 .. autoclass:: ferris.User
     :members:
     :inherited-members:
 
 Utility Functions
 -----------------
+
 .. autofunction:: ferris.get_snowflake_creation_date
 
 Enumerations
 ------------
+
 .. class:: ModelType
     Specifics a model's type.
+
     .. attribute:: Guild
         A guild model
+
     .. attribute:: User
         A user model
+
     .. attribute:: Channel
         A channel model
+
     .. attribute:: Member
         A member model
 
 
+Exceptions
+----------
+
+.. automodule:: ferris.errors
+    :members:
+
+
+
 Exception Hierarchy
 -------------------
+
 - :exc:`Exception`
     - :exc:`ferris.FerrisException`
         - :exc:`ferris.HTTPException`
--- ferris/errors.py
+++ ferris/errors.py
@@ -11,32 +11,57 @@ __all__ = (
 
 
 class FerrisException(Exception):
+    """
+    Base class for all ferris exceptions.
+    Theroically, you can use this exception to catch all ferris exceptions.
+    """
     pass
 
 
 class HTTPException(FerrisException):
+    """
+    Base class for all ferris HTTP exceptions.
+
+    Attributes
+    ----------
+    status: int
+        The status code of the response.
+    resp: :class:`aiohttp.ClientResponse`
+        The response object.
+    """
     def __init__(self, resp: ClientResponse, content: str = None):
         content = content or resp.reason
-        self.status_code = resp.status
+        self.status = resp.status
         self.resp = resp
         super().__init__(content)
 
 
 class BadRequest(HTTPException):
+    """The request was invalid or cannot be otherwise served."""
     pass
 
 
 class Unauthorized(HTTPException):
+    """The request requires user authentication."""
     pass
 
 
 class Forbidden(HTTPException):
+    """
+    The request was a valid request, 
+    but the server is refusing to respond to it, as you don't have enough permissions to do that.
+    """
     pass
 
 
 class NotFound(HTTPException):
+    """The server cannot find what you requesed for."""
     pass
 
 
 class FerrisUnavailable(HTTPException):
+    """
+    SSHHH! Ferris is sleeping, and can't answer the request right now. 
+    Try again later.
+    """
     pass
