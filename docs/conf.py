8f44628564f8bf950dc1da836ef6cd5a5eb3d7cd
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-20 15:39:38 -0700

    right doc need that

--- docs/conf.py
+++ docs/conf.py
@@ -76,6 +76,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
+html_static_path = ["_static"]
+
 html_favicon = 'ferriswheel.svg'
 html_logo = 'ferriswheel.svg'
 
@@ -106,5 +108,3 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["./_static"]
-
-html_static_path = ["_static"]
