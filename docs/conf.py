8f53d5e8730bd26e54c0e7b33343df1f4eced395
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-21 13:43:42 -0700

    Groupwise for autodoc member order

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,8 @@ extensions = [
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+autodoc_member_order = "groupwise"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
