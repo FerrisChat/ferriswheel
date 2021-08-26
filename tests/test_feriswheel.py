041c48a126957b6777eb6075a23dc543b9009a3b
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-26 15:03:52 -0700

    Add tests

---
+++ tests/test_feriswheel.py
@@ -0,0 +1,41 @@
+import os
+import logging
+
+import ferris
+
+log = logging.getLogger(__name__)
+
+_log = logging.getLogger()
+_log.addHandler(logging.StreamHandler())
+_log.setLevel(logging.DEBUG)
+
+
+
+class Client(ferris.Client):
+    async def on_login(self):
+        log.info("Starting test.")
+
+        c = await client.fetch_channel(962487915116318470726993575936)
+        log.info(repr(c))
+
+        m = await c.send("Test.")
+        log.info(repr(m))
+
+        log.info("Fetch channel and send message works.")
+
+        u = await client.fetch_user(962483372771538950023163150336)
+        log.info(repr(u))
+        log.info("Fetch user works.")
+
+        g = await client.fetch_guild(962487888165481259451216297984)
+        log.info(repr(g))
+        log.info("Fetch guild works.")
+
+        log.info("Test done, all passed")
+
+        await self.close()
+
+
+client = Client()
+
+client.run(email="test.ferriswheel@ferris.chat", password=os.getenv('PASSWORD'), id=962483372771538950023163150336)
