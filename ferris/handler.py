1d12b080223faa42b3924737726e82db816819ca
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-09-14 20:42:00 -0700

    I forgot to add it

---
+++ ferris/handler.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Coroutine
+from .user import User
+from .message import Message
+from .channel import Channel
+from .guild import Guild
+from .member import Member
+
+if TYPE_CHECKING:
+    from .connection import Connection
+
+
+class _BaseEventHandler:
+    def __init__(self, connection: Connection) -> None:
+        self.connection: Connection = connection
+        self.dispatch: Coroutine = connection.dispatch
+
+    def handle(self, _data: dict):
+        event = _data.get('c')
+        data = _data.get('d')
+
+        self.connection.loop.create_task(getattr(self, event)(data))
+
+
+class EventHandler(_BaseEventHandler):
+    async def IdentifyAccepted(self, data):
+        u = User(self.connection, data.get('user', {}))
+
+        self.connection._user = u
+
+    async def MessageCreate(self, data):
+        m = Message(self.connection, data.get('message'))
+        await self.dispatch('message', m)
+        self.connection.store_message(m)
+
+    async def MessageUpdate(self, data):
+        old = Message(self.connection, data.get('old'))
+
+        if new := self.connection.get_message(old.id):
+            new._process_data(data.get('new'))
+        else:
+            new = Message(self.connection, data.get('new'))
+
+        await self.dispatch('message_edit', old, new)
+
+    async def MessageDelete(self, data):
+        if m := self.connection.get_message(data.get('id')):
+            self.connection._messages.remove(m)
+        else:
+            m = Message(self.connection, data.get('message'))
+        await self.dispatch('message_delete', m)
+
+        self.connection.remove_message(m.id)
+
+    async def ChannelCreate(self, data):
+        if c := self._connection.get_channel(data.get('id')):
+            c._process_data(data.get('channel'))
+        else:
+            c = Channel(self.connection, data.get('channel'))
+            self.connection.store_channel(c)
+        await self.dispatch('channel_create', c)
+
+    async def ChannelUpdate(self, data):
+        old = Channel(self.connection, data.get('old'))
+        if old.id in self.connection._channels:
+            new = self.connection._channels[old.id]
+            new._process_data(data.get('new'))
+        else:
+            self.connection._channels[old.id] = new = Channel(
+                self.connection, data.get('new')
+            )
+
+        await self.dispatch('channel_update', old, new)
+
+    async def ChannelDelete(self, data):
+        c = Channel(self.connection, data.get('channel'))
+        await self.dispatch('channel_delete', c)
+
+        self.connection._channels.pop(c.id, None)
+
+    async def MemberCreate(self, data):
+        guild: Guild = self._connection.get_guild(data.get('guild_id'))
+        if member := guild._members.get(data.get('user_id')):
+            member._process_data(data)
+        else:
+            member = Member(guild, data.get('member'))
+            guild._members[member.id] = member
+        await self.dispatch('member_create', member)
+
+    async def MemberUpdate(self, data):
+        guild: Guild = self._connection.get_guild(data.get('guild_id'))
+        if member := guild._members.get(data.get('user_id')):
+            member._process_data(data.get('member'))
+        else:
+            member = Member(guild, data.get('user'))
+            guild._members[member.id] = member
+        await self.dispatch('member_update', member)
+
+    async def MemberDelete(self, data):
+        guild: Guild = self._connection.get_guild(data.get('guild_id'))
+        if member := guild._members.get(data.get('user_id')):
+            guild._members.pop(member.id, None)
+        else:
+            member = Member(guild, data.get('member'))
+        await self.dispatch('member_delete', member)
+
+    async def UserCreate(self, data):
+        if u := self._connection.get_user(data.get('id')):
+            u._process_data(data.get('user'))
+        else:
+            u = User(self.connection, data.get('user'))
+            self.connection.store_user(u)
+        await self.dispatch('user_create', u)
