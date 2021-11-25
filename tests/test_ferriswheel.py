dbd74c3787c40f653ffcb868138ab706efdbc754
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-24 17:59:02 -0800

    a

--- tests/test_ferriswheel.py
+++ tests/test_ferriswheel.py
@@ -13,61 +13,59 @@ _log.setLevel(logging.DEBUG)
 
 class Client(ferris.Client):
     async def on_ready(self):
-        try:
-            log.info("Starting test.")
-            g = None
-            c = None
-            m = None
+        log.info("Starting test.")
+        g = None
+        c = None
+        m = None
 
-            async def test_guild():
-                g = await self.create_guild(name='test')
+        async def test_guild():
+            g = await self.create_guild(name='test')
 
-                g = await self.fetch_guild(g.id)
-                log.info(repr(g))
-                await g.edit(name='test_edit')
-                log.info("Create and Fetch and edit guild works.")
+            g = await self.fetch_guild(g.id)
+            log.info(repr(g))
+            await g.edit(name='test_edit')
+            log.info("Create and Fetch and edit guild works.")
+            asyncio.create_task(test_invite())
+        
+            c = await g.create_channel(name='test')
+
+            c = await self.fetch_channel(c.id)
+            log.info(repr(c))
+            await c.edit(name='test_edit')
+
+            m = await c.send("Test.")
+            log.info(repr(m))
+            await m.edit(content='test_edit')
             
-                c = await g.create_channel(name='test')
+            log.info("Create, Fetch, Edit channel, send, edit message works.")
+        
+        async def test_invite():
+            i = await g.create_invite()
+            log.info(repr(i))
+            
+            log.info("Create invite works.")
 
-                c = await self.fetch_channel(c.id)
-                log.info(repr(c))
-                await c.edit(name='test_edit')
+        async def test_user():
+            u = await self.fetch_user(self.user.id)
+            log.info(repr(u))
+            log.info("Fetch user works.")
 
-                m = await c.send("Test.")
-                log.info(repr(m))
-                await m.edit(content='test_edit')
-                
-                log.info("Create, Fetch, Edit channel, send, edit message works.")
+        async def test_delete():
+            await m.delete()
+            await c.delete()
+            await g.delete()
+            
+            log.info("Delete message, channel, guild works.")
 
-            async def test_user():
-                u = await self.fetch_user(self.user.id)
-                log.info(repr(u))
-                log.info("Fetch user works.")
+        f = await asyncio.gather(test_guild(), test_user())
 
-            async def test_invite():
-                i = await g.create_invite()
-                log.info(repr(i))
-                
-                log.info("Create invite works.")
+        await test_delete()
 
-            async def test_delete():
-                await m.delete()
-                await c.delete()
-                await g.delete()
-                
-                log.info("Delete message, channel, guild works.")
+        for r in f:
+            if r is not None:
+                raise r
 
-            f = await asyncio.gather(test_guild(), test_user(), test_invite())
-
-            await test_delete()
-
-            for r in f:
-                if r is not None:
-                    raise r
-
-            log.info("Test done, all passed")
-        finally:
-            await self.close()
+        log.info("Test done, all passed")
 
 
 client = Client()
