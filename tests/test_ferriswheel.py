9615f1ea5481f2401dfd3d82cc4a978e509ecbe2
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-24 18:12:08 -0800

    The speed is roughly the same with gather reverted to non gather version

--- tests/test_ferriswheel.py
+++ tests/test_ferriswheel.py
@@ -1,9 +1,7 @@
-import asyncio
-import logging
 import os
+import logging
 
 import ferris
-
 
 log = logging.getLogger(__name__)
 
@@ -17,61 +15,41 @@ class Client(ferris.Client):
         try:
             log.info("Starting test.")
 
+            g = await self.create_guild(name='test')
 
-            async def test_guild():
-                global g
-                global c
-                global m
-                global t
+            g = await self.fetch_guild(g.id)
+            log.info(repr(g))
+            await g.edit(name='test_edit')
+            log.info("Create and Fetch and edit guild works.")
 
-                g = await self.create_guild(name='test')
+            c = await g.create_channel(name='test')
 
-                g = await self.fetch_guild(g.id)
-                log.info(repr(g))
-                await g.edit(name='test_edit')
-                log.info("Create and Fetch and edit guild works.")
-                t = asyncio.create_task(test_invite())
+            c = await self.fetch_channel(c.id)
+            log.info(repr(c))
+            await c.edit(name='test_edit')
 
-                c = await g.create_channel(name='test')
+            m = await c.send("Test.")
+            log.info(repr(m))
+            await m.edit(content='test_edit')
 
-                c = await self.fetch_channel(c.id)
-                log.info(repr(c))
-                await c.edit(name='test_edit')
 
-                m = await c.send("Test.")
-                log.info(repr(m))
-                await m.edit(content='test_edit')
-                
-                log.info("Create, Fetch, Edit channel, send, edit message works.")
+            log.info("Create, Fetch, Edit channel, send, edit message works.")
 
-            async def test_invite():
-                i = await g.create_invite()
-                log.info(repr(i))
-                
-                log.info("Create invite works.")
+            u = await self.fetch_user(self.user.id)
+            log.info(repr(u))
+            log.info("Fetch user works.")
 
-            async def test_user():
-                u = await self.fetch_user(self.user.id)
-                log.info(repr(u))
-                log.info("Fetch user works.")
+            i = await g.create_invite()
+            log.info(repr(i))
 
-            async def test_delete():
-                await m.delete()
-                await c.delete()
-                await g.delete()
-                
-                log.info("Delete message, channel, guild works.")
+            log.info("Create invite works.")
 
-            f = await asyncio.gather(test_guild(), test_user())
+            await m.delete()
+            await c.delete()
+            await g.delete()
 
-            await test_delete()
 
-            for r in f:
-                if r is not None:
-                    raise r
-
-            if t.exception() is not None:
-                raise t.exception()
+            log.info("Delete message, channel, guild works.")
 
             log.info("Test done, all passed")
         except Exception as e:
