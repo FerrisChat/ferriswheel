55e10216d263b0df652fd091798dcd937c27b055
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-24 16:45:50 -0800

    Merge from https://github.com/jay3332/command-handler

--- ferris/plugins/commands/models.py
+++ ferris/plugins/commands/models.py
@@ -1,34 +1,45 @@
 from __future__ import annotations
 
+import inspect
+
 from typing import (
     Any,
     Awaitable,
+    Callable,
+    Collection,
+    Dict,
     Generic,
     List,
     Optional,
+    Tuple,
     TYPE_CHECKING,
     TypeVar,
-    Union,
 )
+
+from .parser import Parser
+from ...message import Message
 
 R = TypeVar('R')
 
 if TYPE_CHECKING:
     from typing_extensions import Concatenate, ParamSpec
 
-    from ferris.channel import Channel
-    from ferris.guild import Guild
-    from ferris.message import Message
-    from ferris.member import Member
-    from ferris.user import User
+    from .core import Bot
+    from .parser import StringReader
 
     P = ParamSpec('P')
-    CommandCallbackT = Callable[Concatenate['Context', P], Awaitable[R]]
+    CommandCallback = Callable[Concatenate['Context', P], Awaitable[R]]
+    ErrorCallback = Callable[['Context', Exception], Awaitable[Any]]
 else:
     P = TypeVar('P')
 
+__all__ = (
+    'Command',
+    'Context',
+)
 
-class Command(Generic[P, R]):
+
+class Command(Parser, Generic[P, R]):
     """Represents a command.
 
     Attributes
@@ -37,97 +48,236 @@ class Command(Generic[P, R]):
         The name of this command.
     aliases: List[str]
         A list of aliases for this command.
-    callback: Callable[Concatenate[:class:`.Context`, P], Awaitable[R]]
-        The command callback for this command.
+    usage: str
+        The custom usage string for this command, or ``None``.
+        See :attr:`~.Command.signature` for an auto-generated version.
+    error_callback: Optional[Callable[[:class:`~.Context`, Exception], Any]]
+        The callback for when an error is raise during command invokation.
+        This could be ``None``.
     """
 
     def __init__(
-        self, name: str, aliases: List[str], callback: CommandCallbackT
+        self,
+        callback: CommandCallback,
+        *,
+        name: str,
+        aliases: Collection[str],
+        brief: Optional[str] = None,
+        description: Optional[str] = None,
+        usage: Optional[str] = None,
+        **attrs
     ) -> None:
+        self.error_callback: Optional[ErrorCallback] = None
+
         self.name: str = name
-        self.aliases: List[str] = aliases
-        self.callback: CommandCallbackT = callback
-        self.on_error: Optional[Callable[[Context, Exception], Any]] = None
+        self.aliases: List[str] = list(aliases)
+        self.usage: Optional[str] = usage
 
-    def error(self, func: Callable[[Context, Exception], Any]) -> None:
-        """Adds an error handler to this command.
+        self._brief: Optional[str] = brief
+        self._description: Optional[str] = description
 
-        Example
-        -------
-        .. code:: python3
+        self._metadata: Dict[str, Any] = attrs
 
-            @bot.command()
-            async def raise_error(ctx: Context) -> None:
-                int('a')  # Will raise ValueError
+        super().__init__()
+        self.overload(callback)
 
-            @raise_error.error
-            async def error_handler(ctx: Context, exc: Exception) -> None:
-                if isinstance(exc, ValueError):
-                    await ctx.send('Got ValueError!')
+    def __str__(self) -> str:
+        return self.qualified_name
+
+    def __repr__(self) -> str:
+        return f'<Command name={self.name!r} brief={self.brief!r}>'
+
+    def __hash__(self) -> int:
+        return hash(self.qualified_name)
+
+    @property
+    def brief(self) -> str:
+        """str: A short description of this command.
+        
+        If no brief is set, the first line of the :attr:`~.Command.description` 
+        is used instead.
         """
-        self.on_error = func
+        if self._brief is not None:
+            return self._brief
 
-    async def invoke(self, ctx: Context, *args: P.args, **kwargs: P.kwargs) -> None:
-        """Invokes this command.
+        try:
+            return self.description.splitlines()[0]
+        except IndexError:
+            return ''
+
+    @property
+    def description(self) -> str:
+        """str: A detailed description of this command.
+        
+        If no description is set, the command's callback docstring is used instead.
+        If no docstring exists, an empty string is returned.
+        """
+        if self._description is not None:
+            return self._description
+
+        doc = self.callback.__doc__
+        if not doc:
+            return ''
+
+        return inspect.cleandoc(doc)
+
+    @property
+    def qualified_name(self) -> str:
+        """str: The qualified name for this command."""
+        return self.name
+
+    @property
+    def signature(self) -> str:
+        """str: The signature, or "usage string" of this command.
+
+        If :attr:`~.Command.usage` is set, it will be returned.
+        Else, it is automatically generated.
+        """
+        return self.usage or super().signature
+
+    def error(self, func: ErrorCallback) -> None:
+        """Registers an error handler for this command.
+        If no errors are raised here, the error is suppressed.
+        Else, `on_command_error` is raised.
+
+        Function signature should be of ``async (ctx: Context, error: Exception) -> Any``.
+        """
+        self.error_callback = func
+
+    async def execute(self, ctx: Context, content: str) -> None:
+        """|coro|
+
+        Parses and executes this command with the given context and argument content.
+
+        .. note::
+            The prefix and command must be removed from content before-hand.
 
         Parameters
         ----------
-        ctx: :class:`.Context`
-            The context to invoke the command with.
-        *args: P.args
-            The positional arguments to pass into the command callback.
-        **kwargs: P.kwargs
-            The keyword arguments to pass into the command callback.
+        ctx: :class:`~.Context`
+            The context to invoke this command with.
+        content: str
+            The content of the arguments to parse.
         """
         try:
-            await self.callback(ctx, *args, **kwargs)
-            # dispatch `on_command` here...
+            ctx.callback, _, _ = await self._parse(content, ctx=ctx)
         except Exception as exc:
-            if self.on_error:
+            if self.error_callback:
                 try:
-                    await self.on_error(ctx, exc)
+                    await self.error_callback(ctx, exc)
                 except Exception as new_exc:
                     exc = new_exc
                 else:
                     return
-            # dispatch `on_command_error` here...
+
+            ctx.bot.dispatch('command_error', ctx, exc)
         else:
-            # dispatch `on_command_success` here...
-            ...
+            await ctx.reinvoke()
+
+    async def invoke(self, ctx: Context, /, *args: P.args, **kwargs: P.kwargs) -> None:
+        """|coro|
+
+        Invokes this command with the given context.
+
+        Parameters
+        ----------
+        ctx: :class:`~.Context`
+            The context to invoke this command with.
+        *args
+            The positional arguments to pass into the callback.
+        **kwargs
+            The keyword arguments to pass into the callback.
+        """
+        try:
+            ctx.bot.dispatch('command', ctx)
+            await ctx.callback(ctx, *args, **kwargs)
+        except Exception as exc:
+            if self.error_callback:
+                try:
+                    await self.error_callback(ctx, exc)
+                except Exception as new_exc:
+                    exc = new_exc
+                else:
+                    return
+
+            ctx.bot.dispatch('command_error', ctx, exc)
+        else:
+            ctx.bot.dispatch('command_success', ctx)
         finally:
-            # dispatch `on_command_complete` here...
-            ...
+            ctx.bot.dispatch('command_complete', ctx)
 
 
 class Context:
-    """Represents the context for a command.
+    """Represents the context for when a command is invoked.
 
     Attributes
     ----------
-    command: Optional[:class:`.Command`]
-        The command invoked. May be ``None``.
+    bot: :class:`~.Bot`
+        The bot that created this context.
+    message: :class:`~.Message`
+        The message that invoked this context.
+    prefix: str
+        The prefix used to invoke this command.
+        Could be, but rarely is ``None``.
+    invoked_with: str
+        The command name used to invoke this command.
+        This could be used to determine which alias invoked this command.
+        Could be ``None``.
+    command: :class:`~.Command`
+        The command invoked. Could be ``None``.
+    callback: Callable
+        The parsed command callback that will be used. Could be ``None``.
+    args: tuple
+        The arguments used to invoke this command. Could be ``None``.
+    kwargs: Dict[str, Any]
+        The keyword arguments used to invoke this command. Could be ``None``.
+    reader: :class:`~.StringReader`
+        The string-reader that was used to parse this command. Could be ``None``.
     """
 
-    def __init__(self, *, message: Message) -> None:
-        self._message: Message = message
-        self.command: Command = None
+    def __init__(self, bot: Bot, message: Message) -> None:
+        self.bot: Bot = bot
+        self.message: Message = message
 
-    @property
-    def message(self) -> Message:
-        """:class:`~.Message`: The invocation message for this context."""
-        return self._message
+        self.prefix: Optional[str] = None
+        self.invoked_with: Optional[str] = None
 
-    @property
-    def author(self) -> Union[User, Member]:
-        """Union[:class:`~.User`, :class:`~.Member`]: The author of this context."""
-        return self._message.author
+        self.command: Optional[Command] = None
+        self.callback: Optional[CommandCallback] = None
+        self.args: Optional[tuple] = None
+        self.kwargs: Optional[Dict[str, Any]] = None
+        self.reader: Optional[StringReader] = None
 
-    @property
-    def channel(self) -> Channel:
-        """:class:`~.Channel`: The channel of this context."""
-        return self._message.channel
+    def __repr__(self) -> str:
+        return f'<Context command={self.command!r}>'
 
-    @property
-    def guild(self) -> Guild:
-        """:class:`~.Guild`: The guild of this context."""
-        return self._message.guild
+    async def invoke(self, command: Command[P, Any], /, *args: P.args, **kwargs: P.kwargs) -> None:
+        """|coro|
+
+        Invokes the given command with this context.
+
+        .. note:: No checks will be called here.
+
+        Parameters
+        ----------
+        command: :class:`~.Command`
+            The context to invoke this command with.
+        *args
+            The positional arguments to pass into the callback.
+        **kwargs
+            The keyword arguments to pass into the callback.
+        """
+        self.command = command
+        self.args = args
+        self.kwargs = kwargs
+
+        await command.invoke(self, *args, **kwargs)
+
+    async def reinvoke(self) -> None:
+        """|coro|
+
+        Re-invokes this command with the same arguments.
+
+        .. note:: No checks will be called here.
+        """
+        await self.invoke(self.command, *self.args, **self.kwargs)
--- ferris/utils.py
+++ ferris/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Any, Callable, Iterable, Optional, TypeVar
+from typing import Any, Callable, Iterable, Optional, TypeVar, TYPE_CHECKING, Awaitable, Union, overload
 
 from .types import Id, Snowflake
 
@@ -9,7 +9,18 @@ __all__ = (
     'get_snowflake_creation_date',
     'find',
     'dt_to_snowflake',
+    'ensure_async',
+    'to_error_string',
 )
+
+
+if TYPE_CHECKING:
+    from typing_extensions import ParamSpec
+    P = ParamSpec('P')
+else:
+    P = TypeVar('P')
+
+R = TypeVar('R')
 
 T = TypeVar('T', covariant=True)
 
@@ -124,3 +135,41 @@ def find(predicate: Callable[[T], Any], iterable: Iterable[T]) -> Optional[T]:
         if predicate(element):
             return element
     return None
+
+@overload
+def ensure_async(func: Callable[P, Awaitable[R]], /) -> Callable[P, Awaitable[R]]:
+    ...
+
+
+@overload
+def ensure_async(func: Callable[P, R], /) -> Callable[P, Awaitable[R]]:
+    ...
+
+
+def ensure_async(func: Union[Callable[P, Awaitable[R]], Callable[P, R]], /) -> Callable[P, Awaitable[R]]:
+    """Ensures that the given function is asynchronous.
+    In other terms, if the function is already async, it will stay the same.
+    Else, it will be converted into an async function. (Note that it will still be ran synchronously.)
+    """
+    @functools.wraps(func)
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        maybe_coro = func(*args, **kwargs)
+
+        if inspect.isawaitable(maybe_coro):
+            return await maybe_coro
+
+        return maybe_coro
+
+    return wrapper
+
+
+def to_error_string(exc: Exception, /) -> str:
+    """Formats the given error into ``{error name}: {error text}``,
+    e.g. ``ValueError: invalid literal for int() with base 10: 'a'``.
+    If no error text exists, only the error name will be returned,
+    e.g. just ``ValueError``.
+    """
+    if str(exc).strip():
+        return '{0.__class__.__name__}: {0}'.format(exc)
+    else:
+        return exc.__class__.__name__
